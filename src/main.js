6b363787f1362cdfa04681f55ddef78f0cf320ac
Lee <gitshuang@users.noreply.github.com>
2024-03-12 23:57:58 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import { Message } from 'personal-component'
+Vue.component(Message.name, Message);
 Vue.config.productionTip = false
 
 new Vue({
